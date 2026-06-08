c6c41723a3428fcc9d0466d808950c2dc4788066
Lawsome103 <lawsomeaudy@gmail.com>
2026-06-08 18:51:26 +0100

    Fix: Export Player class, add draw method, make move parameter optional, add radius property

--- player.ts
+++ player.ts
@@ -1,16 +1,27 @@
-class Player {
+export class Player {
     name: string;
     position: { x: number; y: number };
     score: number;
+    radius: number;
 
     constructor(name: string, position: { x: number; y: number }) {
         this.name = name;
         this.position = position;
         this.score = 0;
+        this.radius = 10;
     }
 
-    move(newPosition: { x: number; y: number }) {
-        this.position = newPosition;
+    move(newPosition?: { x: number; y: number }) {
+        if (newPosition) {
+            this.position = newPosition;
+        }
+    }
+
+    draw(context: CanvasRenderingContext2D) {
+        context.fillStyle = "blue";
+        context.beginPath();
+        context.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2);
+        context.fill();
     }
 
     kickBall() {
